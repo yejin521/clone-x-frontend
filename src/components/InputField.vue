a1f7f9b53210f99bf65bb366c38f772848da471d
Kim Yejin <kimyaejin0521@gmail.com>
2026-05-30 11:26:37 +0900

    로그인페이지 최종

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <input :type="type" :placeholder="placeholder" :value="modelValue" @input="updateValue"/>
+  <div class="container">
+    <input class="input-form" :type="type" :placeholder="placeholder" :value="modelValue" @input="updateValue"/>
   </div>
 </template>
 
@@ -22,6 +22,21 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.container {
+    height: fit-content;
+    width: 300px;
+}
 
+.input-form {
+    box-sizing: border-box;
+    width: 100%;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid gray;
+    background-color: #000000;
+    margin-bottom: 10px;
+    font-size: 12px;
+    color: gray;
+}
 </style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -4,10 +4,10 @@
         <InputField :type="'text'" :placeholder="'Email'" :modelValue="email" v-model="email"/>
         <InputField :type="'password'" :placeholder="'Password'" :modelValue="password" />
 
-        <button @click="login">로그인하기</button>
+        <button class="login-button" @click="login">로그인하기</button>
         <div>
             계정이 없으신가요?
-            <span>가입하기</span>
+            <span class="signup-button">가입하기</span>
         </div>
     </div>
 </template>
@@ -33,6 +33,20 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.login-button {
+    border-radius: 20px;
+    border: 1px solid white;
+    font-size: 15px;
+    font-weight: bold;
+    margin: 10px 0px;
+    padding: 10px;
+    width: 100%;
+    cursor: pointer;
+}
 
+.signup-button {
+    color: aqua;
+    cursor: pointer;
+}
 </style>
